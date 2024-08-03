2e7a456bf34cdf02851ec2e9213bc56ed1f59c76
LikhithDeveloper <likhith931@gmail.com>
2024-08-03 21:29:54 +0530

    Nav bar almost Done

---
+++ index.js
@@ -0,0 +1,7 @@
+addEventListener("DOMContentLoaded",() => {
+    let menu = document.querySelector(".menu-bar");
+    let change = document.querySelector(".responsive-navbar");
+    menu.addEventListener('click',() => {
+        change.classList.toggle('show');
+    });
+});
