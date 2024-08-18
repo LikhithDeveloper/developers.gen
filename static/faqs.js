a096396bf1575479ebbd0d2ebbbcb59dee302ef7
LikhithDeveloper <likhith931@gmail.com>
2024-08-18 18:53:53 +0530

    Done part2

--- static/faqs.js
+++ static/faqs.js
@@ -1,21 +1,32 @@
 addEventListener("DOMContentLoaded", () => {
     let questions = document.querySelectorAll(".faqs");
+    let puls = document.querySelectorAll(".svg");
+    // console.log(questions)
+    // console.log(puls)
     
-    questions.forEach((question) => {
+    let x = questions.forEach((question, index) => {
         question.addEventListener('click', () => {
+            // console.log(index)
+            // console.log(question)
             let answer = question.nextElementSibling;
             let isCurrentlyVisible = answer.classList.contains('answershow');
-            
-            // Close all answers
+
+            // Close all answers and reset plus icons
             let allAnswers = document.querySelectorAll(".faqa1");
             allAnswers.forEach((answer) => {
                 answer.classList.remove('answershow');
             });
+
+            puls.forEach((icon) => {
+                icon.classList.remove('svg1');
+            });
             
-            // Toggle the selected answer, but only if it was not already visible
+            // Toggle the selected answer and corresponding plus icon, but only if it was not already visible
             if (!isCurrentlyVisible) {
                 answer.classList.add('answershow');
+                puls[index].classList.add('svg1');
             }
         });
     });
+
 });
