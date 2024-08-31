6abdb8940b648e68e9cd1d3121c9b9ef917e7755
LikhithDeveloper <likhith931@gmail.com>
2024-08-31 22:06:48 +0530

    website done

--- static/index.js
+++ static/index.js
@@ -1,10 +1,15 @@
-addEventListener("DOMContentLoaded",() => {
+addEventListener("DOMContentLoaded", () => {
+  let menu = document.querySelector(".menu-bar");
+  let change = document.querySelector(".responsive-navbar");
+  menu.addEventListener("click", () => {
+    change.classList.toggle("show");
+  });
+  let clk = document.querySelectorAll(".res-bar");
 
-    let menu = document.querySelector(".menu-bar");
-    let change = document.querySelector(".responsive-navbar");
-    menu.addEventListener('click',() => {
-        change.classList.toggle('show');
+  clk.forEach(clks => {
+    clks.addEventListener("click", () => {
+        // console.log("Hi");
+      change.classList.remove("show");
     });
-
-    
+  });
 });
